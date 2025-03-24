00b0775264b9d395a062d256c67bc02e8c8c4c16
Sergei <143517331+SeregaSimba@users.noreply.github.com>
2025-03-24 18:54:44 +0300

    init: task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -17,8 +17,8 @@
  * '01 Jan 1970 00:00:00 UTC' => 0
  * '04 Dec 1995 00:12:00 UTC' => 818035920000
  */
-function dateToTimestamp(/* date */) {
-  throw new Error('Not implemented');
+function dateToTimestamp(date) {
+  return new Date(date) - new Date('01 Jan 1970 00:00:00 UTC');
 }
 
 /**
@@ -31,8 +31,11 @@ function dateToTimestamp(/* date */) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hour = date.getHours().toString().padStart(2, '0');
+  const min = date.getMinutes().toString().padStart(2, '0');
+  const sec = date.getSeconds().toString().padStart(2, '0');
+  return `${hour}:${min}:${sec}`;
 }
 
 /**
@@ -46,8 +49,16 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  return [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ][new Date(date).getUTCDay()];
 }
 
 /**
@@ -61,8 +72,18 @@ function getDayName(/* date */) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const week = new Date(date);
+  const newWeek = week.getUTCDay();
+
+  const resWeek = (5 - newWeek + 7) % 7;
+
+  if (resWeek === 0) {
+    week.setUTCDate(week.getUTCDate() + 7);
+  } else {
+    week.setUTCDate(week.getUTCDate() + resWeek);
+  }
+  return week;
 }
 
 /**
@@ -76,8 +97,8 @@ function getNextFriday(/* date */) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  return new Date(year, month).getUTCDate();
 }
 
 /**
@@ -91,8 +112,12 @@ function getCountDaysInMonth(/* month, year */) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const firstWeek = new Date(dateStart);
+  const secondsWeek = new Date(dateEnd);
+  const resWeek = secondsWeek - firstWeek;
+  const day = resWeek / 1000 / 60 / 60 / 24;
+  return day + 1;
 }
 
 /**
@@ -112,8 +137,12 @@ function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const newDate = new Date(date);
+  const newPerStr = new Date(period.start).getTime();
+  const newPerEnd = new Date(period.end).getTime();
+
+  return newPerStr <= newDate && newDate <= newPerEnd;
 }
 
 /**
@@ -127,8 +156,9 @@ function isDateInPeriod(/* date, period */) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const newDate = new Date(date);
+  return newDate.toLocaleString('en-US', { timeZone: 'UTC' });
 }
 
 /**
@@ -143,8 +173,17 @@ function formatDate(/* date */) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const newDate = new Date(year, month - 1);
+  let obj = 0;
+
+  while (newDate.getMonth() === month - 1) {
+    if ([6, 0].includes(newDate.getDay())) {
+      obj += 1;
+    }
+    newDate.setDate(newDate.getDate() + 1);
+  }
+  return obj;
 }
 
 /**
